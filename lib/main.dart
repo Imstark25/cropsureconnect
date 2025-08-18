e362ac57b0714ff3a8af9c1fea650f7e0d76de96
Jeniston007 <selvasahayamjeniston1@gmail.com>
2025-08-18 14:56:33 +0530

    fix: fix the error

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import
+import 'firebase_options.dart'; // Import generated options
 
-// Placeholder for your Firebase options file
-import 'farmer_views/views/home_screen.dart';
-import 'firebase_options.dart';
-
-Future<void> main() async {
+void main() async {
+  // Ensure Flutter is initialized
   WidgetsFlutterBinding.ensureInitialized();
-  // Initialize Firebase with your project's options
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(CropsureConnectApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Cropsureconnect',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: 'Poppins',
+      ),
+      // home: const DashboardView(),
+    );
+  }
 }
